aaf1f4645a6a55b1e964c8445ba3573bd2315b78
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-09-30 18:02:35 -0500

    Add microphone autodetection for screen recording
    The previous screen recording feature had the microphone name hardcoded. This update enables Flowizi to automatically detect the system's main microphone and configure FFmpeg to use it for audio recording.

--- src/app_core/environment.py
+++ src/app_core/environment.py
@@ -1,7 +1,10 @@
 import pyaudio
 import os
+import re
 import subprocess
+import sounddevice
 from datetime import datetime
+from fuzzywuzzy import fuzz
 
 class Environment():
     def __init__(self, name):
@@ -45,29 +48,34 @@ class Environment():
         ffmpeg_path = os.path.join(package_dir, '..', "..", 'bin', 'ffmpeg_windows.exe')
         return ffmpeg_path
 
-    def get_microphone_list(self):
-        audio = pyaudio.PyAudio()
-        microphones = []
+    def get_main_microphone(self):
+        return sounddevice.query_devices(kind='input')['name']
+    
+    def get_ffmpeg_devices(self):
+        """Returns the devices FFmpeg can detect in the computer. FFmpeg expectes to use one of these devices to record audio"""
+        command = [self.get_ffmpeg_path(), "-list_devices", "true", "-f", "dshow", "-i", "dummy"]
+        vale = subprocess.run(command, stderr=subprocess.PIPE, text=True, encoding = "utf-8")
+        device_names = re.findall(r'"([^"]+)"', vale.stderr)
+        new_list = []
 
-        for index in range(audio.get_device_count()):
-            device_info = audio.get_device_info_by_index(index)
-            if device_info["maxInputChannels"] > 0:
-                microphones.append(device_info["name"])
-        return microphones
+        for i in range(1, len(device_names)):
+            if not i % 2 == 0:
+                new_list.append(device_names[i - 1])
+        return new_list
 
     def start_recording(self):
-        microphone_list = self.get_microphone_list()
-        print(type(microphone_list[1]))
+        main_microphone = self.get_main_microphone()
+        ffmpeg_devices = self.get_ffmpeg_devices()
+        for device in ffmpeg_devices:
+            if fuzz.ratio(main_microphone, device) > 80:
+                record_microphone = device
+
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d %H-%M-%S")
         ffmpeg_path = self.get_ffmpeg_path()
-        print(ffmpeg_path)
-        microphone_name = microphone_list[1] + "udio)"
-        fixed_name = microphone_name.encode('utf-8').decode('utf-8')
-        print(fixed_name)
         file_output_name = f"{self.name} {formatted_datetime}.mp4"  
         command = [ffmpeg_path, "-f", "gdigrab", "-framerate", "60", 
-        "-i", "desktop", "-f", "dshow", "-i", f"audio=Varios micrófonos (Realtek(R) Audio)", 
+        "-i", "desktop", "-f", "dshow", "-i", f"audio={record_microphone}",
         "-c:v" , "libx264", "-preset", "ultrafast", "-pix_fmt", "yuv420p", "-loglevel", "quiet", file_output_name] 
 
         print("Recording screen. Press ctrl + c to stop recording")
