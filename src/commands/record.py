27480df678d44baf7a05e35e0bd2e7a2c12070b2
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-10-11 17:46:13 -0500

    Add record modification feedback
    Provide user feedback when the record command is successfully executed. Previously, the record attribute of an environment was updated without informing the user. Now, users receive a confirmation message indicating the successful update of the record attribute.

--- src/commands/record.py
+++ src/commands/record.py
@@ -12,13 +12,15 @@ def record(args, parser):
 
 def toogle_on(name):
     if not flowizi.json.verify_environment_recording(True, name):
-        print(f"The {name} environment is already set to record the screen")
+        print(f"Error. The {name} environment is set to record the screen")
     else:
         flowizi.json.update_environment_record(True, name)
+        print(f"The {name} environment is now set to record the screen")
 
 
 def toogle_off(name):
     if not flowizi.json.verify_environment_recording(False, name):
-        print(f"The {name} environment is already set not to record the screen")
+        print(f"Error. The {name} environment is set to not record the screen")
     else:
         flowizi.json.update_environment_record(False, name)
+        print(f"The {name} environment is now set to not record the screen")
