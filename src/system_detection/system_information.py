04488f6b13ffc0567ddd1a7e9b6f9352f3f32ded
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-10-13 13:07:50 -0500

    Use known folder IDs instead of hardcoded paths
    Replaced hardcoded paths with known folder IDs in system_information. Previously, the system detected language and selected paths from a hashmap, leading to inefficiency and complexity. This change simplifies the code, improves maintainability, and ensures compatibility across all languages supported by Windows.

--- src/system_detection/system_information.py
+++ src/system_detection/system_information.py
@@ -1,40 +1,28 @@
 import platform
 import os
-import locale
+from ctypes import windll, byref, c_wchar_p
+from uuid import UUID
+from pathlib import Path
 
 operating_system = platform.system()
 user = os.getlogin()
 
-system_locale = locale.getdefaultlocale()
-
-# The language is usually the first element in the tuple
-system_language_code = system_locale[0]
-language = system_language_code[:2]
+VIDEOS_ID = UUID("18989b1d-99b5-455b-841c-ab7c74e4ddfc").bytes_le
+APPDATA_LOCAL = UUID("F1B32785-6FBA-4FCF-9D55-7B8E7F157091").bytes_le
 
 
-def english():
-    values = {"json_dir": f"C:/Users/{user}/AppData/Local/flowizi",
-              "json": f"C:/Users/{user}/AppData/Local/flowizi/data.json",
-              "record_dir": f"C:/Users/{user}/Videos/flowizi"}
-    return values
+def get_known_folder_path(folder_id):
+    path_ptr = c_wchar_p()
+    result = windll.shell32.SHGetKnownFolderPath(folder_id, 0, None, byref(path_ptr))
+    if result != 0:
+        raise OSError(f"Failed to retrieve folder path, error code: {result}")
+    return Path(path_ptr.value)
 
 
-def spanish():
-    values = {"json_dir": f"C:/Usuarios/{user}/AppData/Local/flowizi",
-              "json": f"C:/Usuarios/{user}/AppData/Local/flowizi/data.json",
-              "record_dir": f"C:/Usuarios/{user}/Videos/flowizi"}
-    return values
-
-
-def get_language(language):
-    if language == "en":
-        return english()
-    elif language == "es":
-        return spanish()
-
-
-values = get_language(language)
-json_dir = values["json_dir"]
-json_path = values["json"]
-record_dir = values["record_dir"]
-system_sound_names = ["Stereo Mix", "Mezcla estéreo"]
+json_dir = f"{get_known_folder_path(APPDATA_LOCAL)}/flowizi"
+json_path = f"{json_dir}/data.json"
+record_dir = f"{get_known_folder_path(VIDEOS_ID)}/flowizi"
+system_sound_names = ["Stereo Mix", "Stereo-Mix", "Mezcla estéreo",
+                      "Mixagem estéreo", "Mixage stéréo", "Mix Stereo",
+                      "立体声混音", "ステレオミックス", "스테레오 믹스",
+                      "Стерео Микс"]
