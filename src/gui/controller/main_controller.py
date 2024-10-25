ccecc1030edf0335194db8831e58330054d7a4ee
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-10-25 12:02:25 -0500

    Set up sidebar to display information about the selected environment
    - Added visual feedback by changing label color when an environment is selected.
    - Applied the same color to toolbar buttons on hover.
    - Sidebar now shows key details for the selected environment, including the number of websites, settings, name, etc.

--- src/gui/controller/main_controller.py
+++ src/gui/controller/main_controller.py
@@ -1,6 +1,7 @@
 import sys
 from PyQt5.QtWidgets import QApplication
 from gui.view.main_view import MainWindow
+from flowizi import flowizi
 
 
 class Controller:
@@ -8,4 +9,20 @@ class Controller:
         app = QApplication(sys.argv)
         self.main_view = MainWindow()
         self.main_view.show()
+        self.main_view.label_signal.connect(self.element_clicked)
+
         sys.exit(app.exec_())
+
+    def element_clicked(self, pos):
+        self.main_view.element_info_container.itemAt(1).widget().setText(f"Name: {flowizi.environment_list[pos].name}")
+        self.main_view.element_info_container.itemAt(2).widget().setText(f"Screen recording: {flowizi.environment_list[pos].record}")
+        self.main_view.element_info_container.itemAt(3).widget().setText(f"Websites: {len(flowizi.environment_list[pos].websites)}")
+        self.main_view.element_info_container.itemAt(4).widget().setText(f"Apps: {len(flowizi.environment_list[pos].applications)}")
+        self.main_view.element_info_container.itemAt(5).widget().setText(f"Files: {len(flowizi.environment_list[pos].files)}")
+
+        for i in range(len(self.main_view.grid)):
+            if pos == i:
+                clicked_label = self.main_view.grid.itemAt(pos)
+                clicked_label.widget().setStyleSheet(self.main_view.label_clicked_style)
+            else:
+                self.main_view.grid.itemAt(i).widget().setStyleSheet(self.main_view.label_default_style)
--- src/gui/view/main_view.py
+++ src/gui/view/main_view.py
@@ -3,13 +3,15 @@ import math
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QPushButton, QLabel,
                             QWidget, QVBoxLayout, QSplitter, QHBoxLayout, QGridLayout, QSizePolicy, QLayout)
 from PyQt5.QtGui import QIcon, QFont
-from PyQt5.QtCore import Qt
+from PyQt5.QtCore import Qt, pyqtSignal
 from flowizi import flowizi
 
 
 class MainWindow(QMainWindow):
+    label_signal = pyqtSignal(int)
     def __init__(self):
         super().__init__()
+        # self.label_signal.connect(self.handle_label_click)
         self.resize(1000, 600)
         self.setWindowTitle("Flowizi")
         self.setWindowIcon(QIcon("../assets/logo.svg"))
@@ -22,14 +24,10 @@ class MainWindow(QMainWindow):
         vbox = QVBoxLayout()
         splitter = QSplitter(Qt.Horizontal)
         right_widget = QWidget()
-        element_info_container = QVBoxLayout()
-        right_widget.setLayout(element_info_container)
-        test = QLabel("No environments selected")
-        test.setWordWrap(True)
-        test.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
-        test.setStyleSheet("background-color: #454541; font-size: 20px; color: white; padding: 20px;")
-        element_info_container.addWidget(test)
-
+        right_widget.setStyleSheet("background-color: #454541;")
+        self.generate_element_sidebar()
+        right_widget.setLayout(self.element_info_container)
+        
         start_button = QPushButton("Start")
         start_button.setMaximumWidth(100)
         start_button.setMaximumHeight(40)
@@ -41,14 +39,14 @@ class MainWindow(QMainWindow):
                                         font-size: 20px;
                                     }
                                     QPushButton:hover{
-                                        background-color: green;
+                                        background-color: #f19600;
                                     }""")
         start_button.setStyleSheet("""QPushButton{
                                         color: white;
                                         font-size: 20px;
                                     }
                                     QPushButton:hover{
-                                        background-color: green;
+                                        background-color: #f19600;
                                     }""")
         toolbar = QHBoxLayout()
         toolbar.setContentsMargins(0, 0, 0, 0)
@@ -63,21 +61,47 @@ class MainWindow(QMainWindow):
         splitter.addWidget(right_widget)
         central_widget.setLayout(vbox)
 
+    def generate_element_sidebar(self):
+        style = "background-color: #454541; font-size: 18px; color: white; padding: 20px;"
+        self.element_info_container = QVBoxLayout()
+        env_name = QLabel("No environments selected")
+        env_name.setWordWrap(True)
+        env_name.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Preferred)
+        env_name.setStyleSheet(style)
+        env_name.setFixedHeight(65)
+
+        env_screen_rec_setting = QLabel("")
+        env_screen_rec_setting.setStyleSheet(f"{style} height: 1px;")
+        env_screen_rec_setting.setFixedHeight(60)
+
+        env_num_websites = QLabel("")
+        env_num_websites.setStyleSheet(f"{style} height: 1px;")
+        env_num_websites.setFixedHeight(60)
+
+        env_num_files = QLabel("")
+        env_num_files.setStyleSheet(f"{style} height: 1px;")
+        env_num_files.setFixedHeight(60)
+
+        env_num_apps = QLabel("")
+        env_num_apps.setStyleSheet(f"{style} height: 1px;")
+        env_num_apps.setFixedHeight(60)
+
+        self.element_info_container.addStretch()
+        self.element_info_container.addWidget(env_name)
+        self.element_info_container.addWidget(env_screen_rec_setting)
+        self.element_info_container.addWidget(env_num_websites)
+        self.element_info_container.addWidget(env_num_apps)
+        self.element_info_container.addWidget(env_num_files)
+
+
     def generate_environment_grid(self) -> QWidget:
         number_elements_row = 4
         total_environments = len(flowizi.environment_list)
         total_rows = math.ceil(total_environments/number_elements_row)
         row_number = 0
         current_environment = 0
-
-        grid = QGridLayout()
-        grid.setSpacing(30)
-        grid_widget = QWidget()  # New widget for the grid
-        grid_widget.setLayout(grid)  # Set the grid layout on this widget
-        for row in range(total_rows):
-            for environment in range(number_elements_row):
-                label = QLabel(f"{flowizi.environment_list[current_environment].name}")
-                label.setStyleSheet("""QLabel{
+#f19600;
+        self.label_default_style = """QLabel{
                                     background-color: #454541;
                                     color: white;
                                     font-size: 20px;
@@ -87,17 +111,43 @@ class MainWindow(QMainWindow):
                                     }
                                     QLabel:hover{
                                     background-color: #4d4c49;
-                                    }""")
+                                    }"""
+        self.label_clicked_style =  """QLabel{
+                                    background-color: #f19600;
+                                    color: white;
+                                    font-size: 20px;
+                                    height: 10px;
+                                    border: 2px solid white;
+                                    border-radius: 10px;
+                                    }"""
+
+ 
+        self.grid = QGridLayout()
+        self.grid.setSpacing(30)
+        grid_widget = QWidget()  # New widget for the grid
+        grid_widget.setLayout(self.grid)  # Set the grid layout on this widget
+        for row in range(total_rows):
+            for environment in range(number_elements_row):
+                label = QLabel(f"{flowizi.environment_list[current_environment].name}")
+                label.setStyleSheet(self.label_default_style)
                 label.setAlignment(Qt.AlignCenter)
-                grid.addWidget(label, row, environment)
+                self.grid.addWidget(label, row, environment)
+                label.mousePressEvent = self.create_label_event(self.grid.indexOf(label))
                 current_environment += 1
 
                 if current_environment == total_environments:
                     break
             row_number += 1
-            grid.setRowStretch(row, 1)
+            self.grid.setRowStretch(row, 1)
         return grid_widget
 
+    def create_label_event(self, pos):
+        def event(event):
+            self.label_signal.emit(pos)
+        return event
+
+    #def handle_label_click(self, pos):
+        print(pos)
 
 def main():
     app = QApplication(sys.argv)
