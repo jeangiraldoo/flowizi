a09211f76e42c055c64542dda2f58e3f2f1af22d
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-10-28 22:07:02 -0500

    feat(database): Add functionality to add elements to environments
    - Elements are now added to environments, with results stored in their corresponding 'environment_element' tables (e.g., environment_websites, environment_applications, etc).
    - The addition occurs only if the (environment_id, element_id) tuple is not already present in the table.

--- src/core/database/database.py
+++ src/core/database/database.py
@@ -8,44 +8,55 @@ cursor.execute('''
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL,
         record INTEGER NOT NULL
-    )
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS applications (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL,
         url TEXT UNIQUE NOT NULL
-        )
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS websites (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL,
         url TEXT UNIQUE NOT NULL
-        )
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS files (
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         name TEXT UNIQUE NOT NULL,
         url TEXT UNIQUE NOT NULL
-        )
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS environment_applications (
         environment_id INTEGER,
-        application_id INTEGER,
-        PRIMARY KEY (environment_id, project_id),
+        element_id INTEGER,
+        PRIMARY KEY (environment_id, element_id),
         FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE,
-        FOREIGN KEY (application_id) REFERENCES applications(id) ON DELETE CASCADE
-        )
+        FOREIGN KEY (element_id) REFERENCES applications(id) ON DELETE CASCADE
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS environment_websites (
         environment_id INTEGER,
-        website_id INTEGER,
-        PRIMARY KEY (environment_id, project_id),
+        element_id INTEGER,
+        PRIMARY KEY (environment_id, element_id),
         FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE,
-        FOREIGN KEY (website_id) REFERENCES websites(id) ON DELETE CASCADE
-        )
+        FOREIGN KEY (element_id) REFERENCES websites(id) ON DELETE CASCADE
+)''')
+
+cursor.execute('''
     CREATE TABLE IF NOT EXISTS environment_files (
         environment_id INTEGER,
-        file_id INTEGER,
-        PRIMARY KEY (environment_id, file_id),
-        FOREIGN KEY (environments_id) REFERENCES environments(id) ON DELETE CASCADE,
-        FOREIGN KEY (file_id) REFERENCES files(id) ON DELETE CASCADE
-    )
-''')
+        element_id INTEGER,
+        PRIMARY KEY (environment_id, element_id),
+        FOREIGN KEY (environment_id) REFERENCES environments(id) ON DELETE CASCADE,
+        FOREIGN KEY (element_id) REFERENCES files(id) ON DELETE CASCADE
+)''')
 
 
 def add_environment(name):
@@ -55,6 +66,55 @@ def add_environment(name):
         print(f"The {name} environment was successfully created!")
     except:
         print("Error. There's already an environment with that name.")
+
+
+def insert_element(env_name, element_type, name, url):
+    env_id = get_environment_ID(env_name)
+    if not env_id:
+        print("There's no environment with that name")
+        return False
+
+    element_id = get_element_ID(name, url, element_type)
+
+    insert_result = finish_insert_element(env_id, env_name, element_id, element_type)
+    if not insert_result:
+        print(f"The {name} element is already in the {env_name} environment")
+        return False
+
+    print(f"{name} was succesfully added to the {env_name} environment!")
+    return True
+
+def get_environment_ID(env_name) -> int:
+    try:
+        cursor.execute("SELECT id FROM environments WHERE name = ?", (env_name,))
+        env_result = cursor.fetchone()
+        env_id = env_result[0]
+        return env_id
+    except:
+        return 0
+
+
+def get_element_ID(name, url, element_type) -> int:
+    element = (name, url)
+    try:
+        cursor.execute(f"SELECT id FROM {element_type} where name = ?", (name,))
+        element_result = cursor.fetchone()
+        element_id = element_result[0]
+    except:
+        cursor.execute(f"INSERT INTO {element_type} (name, url) VALUES (?, ?)", element)
+        cursor.execute(f"SELECT id FROM {element_type} where name = ?", (name,))
+        element_result = cursor.fetchone()
+        element_id = element_result[0]
+
+    return element_id
+
+
+def finish_insert_element(env_id, env_name, element_id, element_type):
+    try:
+        cursor.execute(f"INSERT INTO environment_{element_type} (environment_id, element_id) VALUES (?, ?)", (env_id, element_id))
+        return True
+    except:
+        return False
 
 
 def update_environment_record(env_name, value):
