d4354a97585c503a49965d992c49c9d8736a7450
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-10-29 12:51:21 -0500

    feat(db): Add environment and element deletion
    - Environments and elements can now be deleted from the database if
      a matching entity exists with the given attributes.
    - Moved element-insertion logic out of get_element_ID() and created
      insert_and_get_element_ID() to follow SRP, allowing ID retrieval from
      any part of the program.

--- src/core/database/database.py
+++ src/core/database/database.py
@@ -68,13 +68,27 @@ def add_environment(name):
         print("Error. There's already an environment with that name.")
 
 
+def delete_environment(name):
+    id = get_environment_ID(name)
+    print(id)
+    if not id:
+        print("There is no environment with the specified name")
+        return False
+
+    cursor.execute("DELETE FROM environments WHERE id = ?", (id,))
+    print(f"The {name} environment was successfully removed!")
+
+
 def insert_element(env_name, element_type, name, url):
     env_id = get_environment_ID(env_name)
     if not env_id:
         print("There's no environment with that name")
         return False
 
-    element_id = get_element_ID(name, url, element_type)
+    element_id = get_element_ID(name, element_type)
+
+    if not element_id:
+        element_id = insert_and_get_element_ID(name, url, element_type)
 
     insert_result = finish_insert_element(env_id, env_name, element_id, element_type)
     if not insert_result:
@@ -83,6 +97,21 @@ def insert_element(env_name, element_type, name, url):
 
     print(f"{name} was succesfully added to the {env_name} environment!")
     return True
+
+
+def delete_element(env_name, element_type, name):
+    env_id = get_environment_ID(env_name)
+    if not env_id:
+        print("There is no environment with the specified name")
+        return False
+
+    element_id = get_element_ID(name, element_type)
+    if not element_id:
+        print(f"There is no element called {name} in the {env_name} environment")
+
+    cursor.execute(f"DELETE FROM environment_{element_type} WHERE element_id = ?", (element_id,))
+    print("The {name} element was deleted from the {env_name} environment!")
+
 
 def get_environment_ID(env_name) -> int:
     try:
@@ -94,17 +123,22 @@ def get_environment_ID(env_name) -> int:
         return 0
 
 
-def get_element_ID(name, url, element_type) -> int:
-    element = (name, url)
+def get_element_ID(name, element_type) -> int:
     try:
         cursor.execute(f"SELECT id FROM {element_type} where name = ?", (name,))
         element_result = cursor.fetchone()
         element_id = element_result[0]
+        return element_id
     except:
-        cursor.execute(f"INSERT INTO {element_type} (name, url) VALUES (?, ?)", element)
-        cursor.execute(f"SELECT id FROM {element_type} where name = ?", (name,))
-        element_result = cursor.fetchone()
-        element_id = element_result[0]
+        return 0
+ 
+
+def insert_and_get_element_ID(name, url, element_type) -> int:
+    element = (name, url)
+    cursor.execute(f"INSERT INTO {element_type} (name, url) VALUES (?, ?)", element)
+    cursor.execute(f"SELECT id FROM {element_type} where name = ?", (name,))
+    element_result = cursor.fetchone()
+    element_id = element_result[0]
 
     return element_id
 
