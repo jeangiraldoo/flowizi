172bf3379efff47125d41ebf3a47505462b2c2b0
Jean Pierre Giraldo <jeann.giraldo@gmail.com>
2024-11-24 12:25:46 -0500

    refactor(main): Delegate CLI parser setup to a dedicated parser module
    Previously, the main module handled both CLI parser setup and application
    mode selection, violating the Single Responsibility Principle.
    
    This refactor moves the parser setup to a dedicated module, improving
    extensibility and adhering to cleaner separation of concerns. As a result,
    the codebase is now better organized and easier to maintain.

--- src/main.py
+++ src/main.py
@@ -1,54 +1,10 @@
-from argparse import ArgumentParser
 from flowizi import flowizi
-from cli import add, remove, list, system, start, record
+from cli.parser import parsed_arg
 from gui.controllers.controller import Controller
 
-parser = ArgumentParser(prog = "Flowizi", description = "Automates the process of starting up your workflow")
-parser.add_argument("-v", action = "store_true", help = "show the app version", default = False)
-
-subparsers = parser.add_subparsers(dest = "command")
-
-subparser_system = subparsers.add_parser("system", help = "show system-specific information")
-subparser_system.set_defaults(func = system.show_system_info_command)
-
-subparser_add = subparsers.add_parser("add", help = "Add an element (environment, website, application or file)")
-subparser_add.add_argument("name")
-subparser_add.add_argument("-w", "--website", nargs = 1, dest = "w", default = False)
-subparser_add.add_argument("-f", "--file", nargs = 1, dest = "f", default = False)
-subparser_add.add_argument("-a", "--application", dest = "a", action = "store_true")
-subparser_add.set_defaults(func = lambda args: add.add_command(args, subparser_add))
-
-subparser_list = subparsers.add_parser("list", help = "show all the environments added")
-subparser_list.add_argument("-w", "--website", dest = "w", action = "store_true")
-subparser_list.add_argument("-f", "--file", dest = "f", action = "store_true")
-subparser_list.add_argument("-a", "--application", dest = "a", action = "store_true")
-subparser_list.add_argument("name", nargs = "?", default = False)
-subparser_list.set_defaults(func = lambda args: list.list_command(subparser_list, args))
-
-subparser_remove = subparsers.add_parser("remove", help = "remove the selected environment from the system")
-subparser_remove.add_argument("name")
-subparser_remove.add_argument("-w", "--website", type = str, dest = "w", default = False)
-subparser_remove.add_argument("-f", "--file", type = str, dest = "f", default = False)
-subparser_remove.add_argument("-a", "--application", type = str, dest = "a", default = False)
-subparser_remove.set_defaults(func = lambda args: remove.remove_command(args, subparser_remove))
-
-subparser_start_meeting = subparsers.add_parser("start", help = "start the specified environment")
-subparser_start_meeting.add_argument("name")
-subparser_start_meeting.set_defaults(func = lambda args: start.start_command(args, subparser_start_meeting))
-
-subparser_record = subparsers.add_parser("record", help = "toogle on/off screen recording for an environment")
-subparser_record.add_argument("name")
-record_group = subparser_record.add_mutually_exclusive_group(required = True)
-record_group.add_argument("-t", action = "store_true")
-record_group.add_argument("-f", action = "store_true")
-subparser_record.set_defaults(func = lambda args: record.record_command(args, subparser_record))
-
-args = parser.parse_args()
-
-if hasattr(args, "func"):
-    args.func(args)
-elif args.v:
+if hasattr(parsed_arg, "func"):
+    parsed_arg.func(parsed_arg)
+elif parsed_arg.v:
     print(f"Flowizi {flowizi.version}")
-elif args.command is None:
-    controller = Controller()
-    controller.main()
+elif parsed_arg.command is None:
+    controller = Controller().main()
